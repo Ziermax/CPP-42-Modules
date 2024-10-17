53fe52fe1848175de86552ffa7691bef28c156d6
Maximiliano Velazquez <mvelazqu@car5s3.42barcelona.com>
2024-10-18 01:31:36 +0200

    ex00 y gitignore

---
+++ M00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvelazqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 00:10:09 by mvelazqu          #+#    #+#             */
+/*   Updated: 2024/10/18 01:28:06 by mvelazqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+void	ft_toupper_string(char *str)
+{
+	int	i;
+
+	for	(i = 0; str[i]; i++)
+		str[i] = toupper(str[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_toupper_string(argv[i]);
+		std::cout << argv[i];
+		i++;
+		if (i != argc)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}

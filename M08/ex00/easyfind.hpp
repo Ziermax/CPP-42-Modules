b4fac202548f803fd8dfe517458e3a58553b5351
Maximiliano Velazquez <mvelazqu@cbr9s1.42barcelona.com>
2025-08-13 21:33:51 +0200

    done

--- M08/ex00/easyfind.hpp
+++ M08/ex00/easyfind.hpp
@@ -6,15 +6,25 @@
 /*  By: mvelazqu <mvelazqu@student.42barcelona.c     +#+  +:+       +#+       */
 /*                                                 +#+#+#+#+#+   +#+          */
 /*  Created: 2025/08/12 17:44:29 by mvelazqu            #+#    #+#            */
-/*  Updated: 2025/08/12 19:27:03 by mvelazqu           ###   ########.fr      */
+/*  Updated: 2025/08/13 21:33:37 by mvelazqu           ###   ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <stdexception>
+
 template < typename T >
-return Value	easyfind( T const 
+T::const_iterator	easyfind( T const &obj, int val )
+{
+	T;;const_iterator	it;
+
+	it = std::find(obj.begin(), obj.end(), val);
+	if (it == obj.end())
+		throw (std::invalid_argument("Not Found"));
+	return (it);
+}
 
 
 #endif

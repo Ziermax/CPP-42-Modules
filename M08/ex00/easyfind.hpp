379d7c95841f1d3a69232bd82be567ddf2921a73
Maximiliano Velazquez <mvelazqu@cbr9s1.42barcelona.com>
2025-09-01 10:50:22 +0200

    added test in main

--- M08/ex00/easyfind.hpp
+++ M08/ex00/easyfind.hpp
@@ -6,25 +6,37 @@
 /*  By: mvelazqu <mvelazqu@student.42barcelona.c     +#+  +:+       +#+       */
 /*                                                 +#+#+#+#+#+   +#+          */
 /*  Created: 2025/08/12 17:44:29 by mvelazqu            #+#    #+#            */
-/*  Updated: 2025/08/13 21:33:37 by mvelazqu           ###   ########.fr      */
+/*  Updated: 2025/08/20 16:08:02 by mvelazqu           ###   ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <stdexception>
+# include <stdexcept>
 
-template < typename T >
-T::const_iterator	easyfind( T const &obj, int val )
+/*	Hecho para que acepta todos tipos de conteneder y no solo los de integers
+*/
+template < typename Input, typename T >
+Input::const_iterator	easyfind( Input const &obj, T const &val )
 {
-	T;;const_iterator	it;
+	Input::const_iterator	it;
 
 	it = std::find(obj.begin(), obj.end(), val);
 	if (it == obj.end())
 		throw (std::invalid_argument("Not Found"));
 	return (it);
-}
+}/*
 
+template < typename T >
+T::const_iterator	easyfind( T const &obj, int val )
+{
+	T::const_iterator	it;
+
+	it = std::find(obj.begin(), obj.end(), val);
+	if (it == obj.end())
+		throw (std::invalid_argument("Not Found"));
+	return (it);
+}*/
 
 #endif
--- M08/ex00/main.cpp
+++ M08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*  By: mvelazqu <mvelazqu@student.42barcelona.c     +#+  +:+       +#+       */
 /*                                                 +#+#+#+#+#+   +#+          */
 /*  Created: 2025/08/12 17:43:38 by mvelazqu            #+#    #+#            */
-/*  Updated: 2025/08/12 19:23:17 by mvelazqu           ###   ########.fr      */
+/*  Updated: 2025/08/20 16:09:25 by mvelazqu           ###   ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "easyfind.hpp"
 
-std::vector<std::string>::iterator	find(
+/*
+std::vector<std::string>::iterator	easyfind(
 		std::vector<std::string> &vecStr,
 		int i)
 {
@@ -28,19 +30,39 @@ std::vector<std::string>::iterator	find(
 //			break ;
 //	}
 	return (it);
-}
+}*/
 
 int	main(void)
 {
 	std::cout << "Hola Mundo" << std::endl;
 
 	std::vector<std::string>	vecStr;
+	std::vector<int>			vecInt;
 
 	vecStr.push_back("HOla");
 	vecStr.push_back("Mundo");
+	vecInt.push_back(1);
+	vecInt.push_back(2);
 	for (std::vector<std::string>::iterator it = vecStr.begin();
 			it != vecStr.end(); ++it)
 	{
-		std::cout << "Vec[]: " << *it << std::endl;
+		std::cout << "VecStr[]: " << *it << std::endl;
+	}
+	for (std::vector<int>::iterator it = vecInt.begin();
+			it != vecInt.end(); ++it)
+	{
+		std::cout << "VecInt[]: " << *it << std::endl;
+	}
+	try
+	{
+		std::vector<int>::const_iterator
+			itInt = easyfind(vecInt, 2);
+		std::vector<std::string>::const_iterator
+			itStr = easyfind(vecStr, 2);
+		std::cout << "Founded this: " << *itInt << std::endl;
+	}
+	catch (...)
+	{
+		std::cerr << "Error Ocurred: Not founded" << std::endl;
 	}
 }
